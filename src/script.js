901d60d163b7e6c5bd08b9aab12efa901b2b206a
Jpbepu <jpbepu@gmail.com>
2024-02-29 17:07:34 -0300

    script de contagem regressiva documentado

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 
-
+//divisoes para o getTime() em ms
 const tFactor = {
     s: 1000,
     m: 60000,
@@ -12,22 +12,33 @@ const tFactor = {
 
 const counter = setInterval(()=>{
 
+    //data do evento
     const bday = new Date('dec 1, 2024 00:00:00').getTime();
+
+    //data de agora
     const now = new Date().getTime();
+
+    //tempo ate o evento
     const intervalo = bday - now;
 
+    //calculo de unidades de tempo
     var dMonth = Math.floor(intervalo/tFactor.mt);
     var dDay = Math.floor((intervalo % tFactor.mt) / tFactor.d);
     var dHr = Math.floor((intervalo % tFactor.d) / tFactor.h);
     var dMin = Math.floor((intervalo % tFactor.h) /tFactor.m);
     var dSec = Math.floor((intervalo % tFactor.m) /tFactor.s);
 
-
+    //injecao do contador no HTML
     document.querySelector('#counter').innerHTML = (`${dMonth}M, ${dDay}d, ${dHr}hrs, ${dMin}min, ${dSec}s !`);
+
+    //teste para ver se o evento ja passou
+    if (intervalo < 0){
+        clearInterval(counter);
+    }
 
 },1000)
 
-//data do evento
+
 
 
 
