4e77962a0ae1713877ca4f092be3c8752aee3e2b
Jpbepu <jpbepu@gmail.com>
2024-04-12 15:00:15 -0300

    exercicio finalizado

---
+++ src/app.js
@@ -0,0 +1,10 @@
+"use strict";
+function multi(a, b) {
+    return a * b;
+}
+;
+function saudacao(nome) {
+    return `Ola ${nome}`;
+}
+console.log(multi(2, 3));
+console.log(saudacao('Lais'));
---
+++ src/app.ts
@@ -0,0 +1,16 @@
+
+
+
+function multi (a: number, b: number): number{
+    return a*b
+};
+
+
+function saudacao (nome:string): string{
+    return `Ola ${nome}`
+}
+
+
+console.log(multi (2, 3));
+
+console.log(saudacao('Lais'));
