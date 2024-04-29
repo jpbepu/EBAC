2b5644c48a2db499bce71803e45b0f03a26be5c1
Jpbepu <jpbepu@gmail.com>
2024-04-29 12:31:24 -0300

    adicionado a classificacao na tabela de IMC

--- projeto-react/src/App.jsx
+++ projeto-react/src/App.jsx
@@ -8,19 +8,51 @@ function App() {
   const [peso, setPeso] = useState(0);
   const [altura, setAltura] = useState(0);
   const [imc, setImc] = useState(0);
+  const [grau, setGrau] = useState('...');
 
   useEffect( () => {
     console.log('componente montou')
+
+    calcIMC()
+
     console.log(`peso: ${peso}, tipo:${typeof peso}`)
     console.log(`altura:${altura} tipo:${typeof altura}`)
-    calcIMC()
     console.log(`imc:${imc} tipo:${typeof imc}`)
+    console.log(grau)
+
 
   }, [peso, altura])
 
   function calcIMC(){
     setImc((peso*10000)/(altura * altura))
+
+    if ( imc < 18,5 ){
+      setGrau('abaixo do peso')
+    }
+    
+    if (imc > 18,5) {
+      setGrau('com o peso normal')
+    }
+    
+    if (imc > 25){
+      setGrau('com excesso de peso')
+    }
+    
+    if (imc > 30) {
+      setGrau('com Obesidade classe I')
+    }
+    
+    if (imc > 35) {
+      setGrau('com Obesidade classe II')
+    }
+    
+    if (imc >= 40){
+    setGrau('com Obesidade classe III')
+    }
+    
   }
+
+
 
   return (
     <>
@@ -35,8 +67,9 @@ function App() {
     </div>
     <br />
     <div>
-      <h1>Seu IMC e:</h1>
+      <h1>Seu IMC é:</h1>
       <h1>{imc.toFixed(2)}</h1>
+      <h2>Voce está {grau}</h2>
     </div>
 
     </>
